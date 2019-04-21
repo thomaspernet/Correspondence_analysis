dfdceb2bc8aa54763dafbd49320a94458c90ffea
Thomas <t.pernetcoudrier@gmail.com>
2019-04-21 18:21:14 +0200

    Update ca_compute.py

--- CorrespondenceAnalysisPy/correspondence_analysis_computation/ca_compute.py
+++ CorrespondenceAnalysisPy/correspondence_analysis_computation/ca_compute.py
@@ -220,7 +220,7 @@ def row_focus_coordinates(df_x, df_y, variance_explained, export_data = False):
 		'y_circle': np.sin(an),
 		'row_norm':row_norm,
 		'col_norm':col_norm,
-		'saveimg' = plt.savefig('focus.png')
+		'saveimg' : plt.savefig('focus.png')
 	}
 	if export_data == True:
 		return dic_data
